923e1830f1bc8792a544ed1a8329c8ec7fd559ee
jakip <jakipov003@gmail.com>
2023-11-15 12:03:30 +0600

    feature:splash_screen

--- app/src/main/java/com/example/sdustore/MainActivity.kt
+++ app/src/main/java/com/example/sdustore/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.sdustore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.sdustore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+        setContentView(binding.root)
     }
 }
